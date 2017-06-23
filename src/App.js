931fa1767a2cd9f98315560fe56e6b8e460a48af
Frank Iriarte <jurrish@gmail.com>
2017-06-22 17:52:37 -0700

    updated .env and firebase.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
 import React, { Component } from 'react';
+import { database } from './firebase';
 import './App.css';
 
 class App extends Component {
+  constructor(props){
+    //app extends native component from react, but also do my special stuff through super
+    super(props);
+    this.state = {
+      data: null
+    };
+  }
+
+  componentDidMount() {
+    //listen at the root of the tree (ref) for changes
+    database.ref().on('value', () => {
+      console.log('the data changed');
+    });
+  }
+
   render() {
     return (
       <div className="App">
---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import firebase from 'firebase';
+
+  // Initialize Firebase
+
+  //place config object from .env here:
+  
+  firebase.initializeApp(config);
+
+
+export default firebase;
+
+export const database = firebase.database();
